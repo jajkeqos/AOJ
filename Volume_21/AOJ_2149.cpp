f562d7af39889ed9cea3ff03fa43d293fc9fe7d4
upamune <jajkeqos@gmail.com>
2014-12-25 11:11:36 +0900

    End 100

--- Volume_21/AOJ_2149.cpp
+++ Volume_21/AOJ_2149.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int y,frame;
+int slot[10001];
+int n, a, b, c, x,frame;
 
 void init(){
+  for(int i = 0;i < 10001; i++){
+    slot[i] = 0;
+  }
   frame = 0;
 }
 
-int main(void){
+int main(){
 
-  int  N, A, B, C, X;
-
-  while(cin >> N >> A >> B >> C >> X && N){
-    init();
-    int x = X;
-    bool first = true;
-    for(int i = 0; i < N; i++){
-      cin >> y;
-      while(frame <= 10000){
-        if(first && y == X) break;
-        frame++;
-        x = (A * x + B) % C;
-        if(y == x) break;
-        //cout << i << ' ' << "x: " << x << endl;
-        first = false;
+    while(cin >> n, n){
+      init();
+      cin >> a >> b >> c >> x;
+      for(int i = 0; i < n; i++){
+        cin >> slot[i];
       }
+      for(int i = 0; i < n;){
+        if(x == slot[i]){
+          i++;
+          if(i == n) break;
+        }
+        x = (a * x + b) % c;
+        frame++;
+        if(frame > 10000){frame = -1; break;}
+      }
+      cout << frame << endl;
     }
-    if(frame == 10001) frame = -1;
-    cout << frame << endl;
-  }
-
   return 0;
 }
